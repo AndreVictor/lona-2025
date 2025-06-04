664b5d094175a06352eab68cf74feba2a6666e78
André Victor <ola@andre-victor.com>
2025-06-04 20:18:04 -0300

    commit 3 04 de junho 2025

--- src/app/acervo/filmes/[slug]/page.tsx
+++ src/app/acervo/filmes/[slug]/page.tsx
@@ -2,7 +2,8 @@ import PageFilmeAcervo from '@/components/PageFilmeAcervo'
 import {getFilmeAcervo} from '@/utils/getFilmeAcervo'
 import React from 'react'
 
-export default async function page({ params }: { params: { slug: string } }) {
-  const filme = await getFilmeAcervo(params.slug)
-  return <PageFilmeAcervo filme={filme} />
+export default async function page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const filme = await getFilmeAcervo(slug);
+  return <PageFilmeAcervo filme={filme} />;
 }
