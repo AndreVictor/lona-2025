81c42b3270ffff6fcbb253ce8912e05af2b84b66
André Victor <ola@andre-victor.com>
2025-06-05 11:44:29 -0300

    commit 2 05 de junho 2025

--- src/app/territorios/[slug]/page.tsx
+++ src/app/territorios/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import PageTerritorio from '@/components/PageTerritorio'
 import getTerritorio from '@/utils/getTerritorio'
+import React from 'react'
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const territorio = await getTerritorio(params.slug)
+export default async function page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const territorio = await getTerritorio(slug)
   return <PageTerritorio territorio={territorio} />
 }
